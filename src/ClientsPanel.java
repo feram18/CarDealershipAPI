f7400470f5e657f0326c23262568de66d02cfbc0
lramos6 <lramos6@students.towson.edu>
2020-11-28 19:26:33 -0500

    Created several panels to be developed

---
+++ src/ClientsPanel.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+
+import javax.swing.JPanel;
+
+public class ClientsPanel extends JPanel {
+	private Connection connection = null;
+	
+	/**
+	 * Create the panel.
+	 */
+	public ClientsPanel() {
+		
+	}
+
+}
---
+++ src/ServiceTicketsPanel.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+
+import javax.swing.JPanel;
+
+public class ServiceTicketsPanel extends JPanel {
+	private Connection connection = null;
+	
+	/**
+	 * Create the panel.
+	 */
+	public ServiceTicketsPanel() {
+
+	}
+
+}
