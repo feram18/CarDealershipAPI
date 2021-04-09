1fb9131e0c6a6bb6dfe162d955183142eab8a6f2
lramos6 <lramos6@students.towson.edu>
2021-04-08 20:01:09 -0400

    Created Entity and Role enum classes

--- src/main/java/edu/towson/cosc457/CarDealership/misc/Entity.java
+++ src/main/java/edu/towson/cosc457/CarDealership/misc/Entity.java
@@ -1,16 +1,84 @@
 package edu.towson.cosc457.CarDealership.misc;
 
+import java.util.Locale;
+
 public enum Entity {
-    CLIENT,
-    COMMENT,
-    DEPARTMENT,
-    EMPLOYEE,
-    LOCATION,
-    LOT,
-    MANAGER,
-    MECHANIC,
-    SALES_ASSOCIATE,
-    SERVICE_TICKET,
-    SITE_MANAGER,
-    VEHICLE
+    ADDRESS {
+        @Override
+        public String toString() {
+            return super.toString().toLowerCase(Locale.ROOT);
+        }
+    },
+    CLIENT {
+        @Override
+        public String toString() {
+            return super.toString().toLowerCase(Locale.ROOT);
+        }
+    },
+    COMMENT {
+        @Override
+        public String toString() {
+            return super.toString().toLowerCase(Locale.ROOT);
+        }
+    },
+    DEPARTMENT {
+        @Override
+        public String toString() {
+            return super.toString().toLowerCase(Locale.ROOT);
+        }
+    },
+    EMPLOYEE {
+        @Override
+        public String toString() {
+            return super.toString().toLowerCase(Locale.ROOT);
+        }
+    },
+    LOCATION {
+        @Override
+        public String toString() {
+            return super.toString().toLowerCase(Locale.ROOT);
+        }
+    },
+    LOT {
+        @Override
+        public String toString() {
+            return super.toString().toLowerCase(Locale.ROOT);
+        }
+    },
+    MANAGER {
+        @Override
+        public String toString() {
+            return super.toString().toLowerCase(Locale.ROOT);
+        }
+    },
+    MECHANIC {
+        @Override
+        public String toString() {
+            return super.toString().toLowerCase(Locale.ROOT);
+        }
+    },
+    SALES_ASSOCIATE {
+        @Override
+        public String toString() {
+            return super.toString().replace("_", " ").toLowerCase(Locale.ROOT);
+        }
+    },
+    SERVICE_TICKET {
+        @Override
+        public String toString() {
+            return super.toString().replace("_", " ").toLowerCase(Locale.ROOT);
+        }
+    },
+    SITE_MANAGER {
+        @Override
+        public String toString() {
+            return super.toString().replace("_", " ").toLowerCase(Locale.ROOT);
+        }
+    },
+    VEHICLE {
+        @Override
+        public String toString() {
+            return super.toString().toLowerCase(Locale.ROOT);
+        }
+    }
 }
--- src/main/java/edu/towson/cosc457/CarDealership/misc/Role.java
+++ src/main/java/edu/towson/cosc457/CarDealership/misc/Role.java
@@ -3,5 +3,6 @@ package edu.towson.cosc457.CarDealership.misc;
 public enum Role {
     USER,
     MOD,
-    ADMIN
+    ADMIN,
+    DEV
 }
