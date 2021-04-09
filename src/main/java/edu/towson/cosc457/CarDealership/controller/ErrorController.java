6676b6e1cae8d08c83ed4bbfb324340ac2a362ac
lramos6 <lramos6@students.towson.edu>
2021-04-09 18:14:38 -0400

    Created error handling controller

---
+++ src/main/java/edu/towson/cosc457/CarDealership/controller/ErrorController.java
@@ -0,0 +1,31 @@
+package edu.towson.cosc457.CarDealership.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
+
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+    @RequestMapping("*/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (Objects.nonNull(status)) {
+            Integer statusCode = Integer.valueOf(status.toString());
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "404 ERROR";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "500 ERROR";
+            }
+        }
+        return "UNKNOWN ERROR";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return null;
+    }
+}
