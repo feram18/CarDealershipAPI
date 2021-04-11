c133b8ce3b3169946aeb61bf3e8595786749c145
lramos6 <lramos6@students.towson.edu>
2021-04-11 17:43:45 -0400

    Renamed parameters

--- src/main/java/edu/towson/cosc457/CarDealership/exceptions/AlreadyAssignedException.java
+++ src/main/java/edu/towson/cosc457/CarDealership/exceptions/AlreadyAssignedException.java
@@ -3,7 +3,7 @@ package edu.towson.cosc457.CarDealership.exceptions;
 import java.text.MessageFormat;
 
 public class AlreadyAssignedException extends RuntimeException {
-    public AlreadyAssignedException(final String entityA, final Long idA, final String entityB, final Long idB) {
-        super(MessageFormat.format("{0} id: {1} is already assigned to {2} id: {3}", entityA, idA, entityB, idB));
+    public AlreadyAssignedException(final String ownedEntity, final Long ownedId, final String ownerEntity, final Long ownerId) {
+        super(MessageFormat.format("{0} id: {1} is already assigned to {2} id: {3}", ownedEntity, ownedId, ownerEntity, ownerId));
     }
 }
