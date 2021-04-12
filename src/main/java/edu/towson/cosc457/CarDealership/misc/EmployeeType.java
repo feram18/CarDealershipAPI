5c098d8ca8312b422c8d25860f4f847574f2d01c
lramos6 <lramos6@students.towson.edu>
2021-04-12 00:59:09 -0400

    Removed overridden toString() methods

--- src/main/java/edu/towson/cosc457/CarDealership/misc/EmployeeType.java
+++ src/main/java/edu/towson/cosc457/CarDealership/misc/EmployeeType.java
@@ -1,8 +1,28 @@
 package edu.towson.cosc457.CarDealership.misc;
 
 public enum EmployeeType {
-    SALES_ASSOCIATE,
-    MECHANIC,
-    MANAGER,
-    SITE_MANAGER
+    SALES_ASSOCIATE {
+        @Override
+        public String toString() {
+            return "Sales Associate";
+        }
+    },
+    MECHANIC {
+        @Override
+        public String toString() {
+            return "Mechanic";
+        }
+    },
+    MANAGER {
+        @Override
+        public String toString() {
+            return "Manager";
+        }
+    },
+    SITE_MANAGER {
+        @Override
+        public String toString() {
+            return "Site Manager";
+        }
+    }
 }
