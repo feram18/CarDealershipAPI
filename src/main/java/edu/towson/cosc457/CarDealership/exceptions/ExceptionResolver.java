59b133d63f3f527e06c465623be5d455848fa3a9
lramos6 <lramos6@students.towson.edu>
2021-04-27 13:27:56 -0400

    Renamed ControllerAdvice, and added unmapped endpoint exception (404)

--- src/main/java/edu/towson/cosc457/CarDealership/exceptions/ExceptionResolver.java
+++ src/main/java/edu/towson/cosc457/CarDealership/exceptions/ExceptionResolver.java
@@ -1,16 +1,18 @@
 package edu.towson.cosc457.CarDealership.exceptions;
 
 import edu.towson.cosc457.CarDealership.model.dto.ErrorDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
-public class RestExceptionHandler {
+public class ExceptionResolver {
     @ExceptionHandler(value = NotFoundException.class)
     @ResponseBody
-    public ResponseEntity<ErrorDto> handleException(NotFoundException exception) {
+    public ResponseEntity<ErrorDto> handleException(final NotFoundException exception) {
         return ResponseEntity
                 .status(exception.getStatus())
                 .body(ErrorDto.builder().message(exception.getMessage()).build());
@@ -18,9 +20,17 @@ public class RestExceptionHandler {
 
     @ExceptionHandler(value = AlreadyAssignedException.class)
     @ResponseBody
-    public ResponseEntity<ErrorDto> handleException(AlreadyAssignedException exception) {
+    public ResponseEntity<ErrorDto> handleException(final AlreadyAssignedException exception) {
         return ResponseEntity
                 .status(exception.getStatus())
+                .body(ErrorDto.builder().message(exception.getMessage()).build());
+    }
+
+    @ExceptionHandler(value = NoHandlerFoundException.class)
+    @ResponseBody
+    public ResponseEntity<ErrorDto> handleException(final NoHandlerFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
                 .body(ErrorDto.builder().message(exception.getMessage()).build());
     }
 }
