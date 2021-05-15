36bf37aeb4a92d083e9ca0956c6257ade730d153
lramos6 <lramos6@students.towson.edu>
2021-05-15 13:12:27 -0400

    Removed toString() overrides

--- src/main/java/edu/towson/cosc457/CarDealership/misc/EmployeeType.java
+++ src/main/java/edu/towson/cosc457/CarDealership/misc/EmployeeType.java
@@ -1,28 +1,8 @@
 package edu.towson.cosc457.CarDealership.misc;
 
 public enum EmployeeType {
-    SALES_ASSOCIATE {
-        @Override
-        public String toString() {
-            return "Sales Associate";
-        }
-    },
-    MECHANIC {
-        @Override
-        public String toString() {
-            return "Mechanic";
-        }
-    },
-    MANAGER {
-        @Override
-        public String toString() {
-            return "Manager";
-        }
-    },
-    SITE_MANAGER {
-        @Override
-        public String toString() {
-            return "Site Manager";
-        }
-    }
+    SALES_ASSOCIATE,
+    MECHANIC,
+    MANAGER,
+    SITE_MANAGER
 }
